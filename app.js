78c36b802ee74eb99a65e6e1aaff07172c7a0897
Philipp Kaufmann <k18flips@gmail.com>
2020-08-29 14:20:44 +0200

    implementing EJS to render data

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
-const fs = require('fs');
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set("view engine", "ejs");
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000.");
@@ -24,12 +24,9 @@ app.post("/", function(req, res) {
   var request = https.request('https://finnhub.io/api/v1/quote?symbol=' + req.body.symbol + '&token=bspqddnrh5rf33i22k4g', function(response) {
     response.on('data', function(d) {
       var data = JSON.parse(d);
-      fs.readFile(__dirname + "/price.html", function(err, file) {
-        res.write(file);
-        var msg = "Price for " + req.body.symbol + " is " + new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(data.c);
-        res.write('<script type="text/javascript">$("#text").html("' + msg + '");</script>');
-        res.send();
-      });
+      var price = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(data.c);
+      var msg = "Price for " + req.body.symbol + " is " + new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'USD' }).format(data.c);
+      res.render("price", {text: msg});
     });
   });
   request.on('error', (e) => {
